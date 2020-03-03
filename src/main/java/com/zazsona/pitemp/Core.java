12160b511de2ab13aa9d8b19e2280162b27458b5
Zazsona <40435178+Zazsona@users.noreply.github.com>
2020-03-03 18:25:59 +0000

    Initial commit.

---
+++ src/main/java/com/zazsona/pitemp/Core.java
@@ -0,0 +1,16 @@
+package com.zazsona.pitemp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Core extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+    }
+
+    @Override
+    public void onDisable()
+    {
+    }
+}
